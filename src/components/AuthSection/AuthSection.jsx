2a1543f7f133861da2657a52d96c2eebe3bb54f4
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-06-01 22:40:54 +0300

    correct position mainpage

--- src/components/AuthSection/AuthSection.jsx
+++ src/components/AuthSection/AuthSection.jsx
@@ -1,14 +1,16 @@
 import Style from "./AuthSection.module.scss"
 import LogoL from "../../img/MainPage/GOOSE2xLogo.png"
-import LogoS from "../../img/MainPage/GOOSE-Desktop.png"
+import LogoS from "../../img/MainPage/GOOSE-MOBILE.png"
 export const AuthSection = () => {
 
     return <section className={Style.AuthSection}>
+        <div>
         <picture>
             <source media="(min-width: 768px)" srcSet={LogoL} />
             <img src={LogoS} alt="logoGoos" />
           </picture>
         <h1 className={Style.Title}>GooseTrack</h1>
+        </div>
         <div className={Style.wrapperBtn}>
         <button className={Style.Sign}> <p>Sign up</p> </button>
         <button className={Style.LogIn}>Log in</button>
