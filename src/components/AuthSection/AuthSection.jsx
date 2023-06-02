9188e2e9e789c270ecea4fc240c4da32f0ae8122
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-06-02 17:05:30 +0300

    correct logo page

--- src/components/AuthSection/AuthSection.jsx
+++ src/components/AuthSection/AuthSection.jsx
@@ -4,10 +4,10 @@ import LogoS from "../../img/MainPage/GOOSE-MOBILE.png"
 export const AuthSection = () => {
 
     return <section className={Style.AuthSection}>
-        <div>
-        <picture>
+        <div className={Style.wrapperLogo}>
+        <picture>  
             <source media="(min-width: 768px)" srcSet={LogoL} />
-            <img src={LogoS} alt="logoGoos" />
+            <img className={Style.LogoImg} src={LogoL} alt="logoGoos" />
           </picture>
         <h1 className={Style.Title}>GooseTrack</h1>
         </div>
